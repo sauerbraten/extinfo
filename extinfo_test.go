b2c87cc618cfc225dde99bb07914bbe6a597963c
sauerbraten <willing.alexander@googlemail.com>
2012-10-09 23:07:57 +0200

    new local work paths

--- extinfo_test.go
+++ extinfo_test.go
@@ -5,21 +5,21 @@ import (
 )
 
 
-func TestBasicInfo(t *testing.T) {
+func TestGetBasicInfo(t *testing.T) {
 	_, err := GetBasicInfo("sauerleague.org", 10000)
 	if err != nil {
 		t.Fail()
 	}
 }
 
-func TestUptime(t *testing.T) {
+func TestGetUptime(t *testing.T) {
 	_, err := GetUptime("sauerleague.org", 10000)
 	if err != nil {
 		t.Fail()
 	}
 }
 
-func TestPlayerInfo(t *testing.T) {
+func TestGetPlayerInfo(t *testing.T) {
 	_, err := GetPlayerInfo("sauerleague.org", 10000, 14)
 	if err != nil {
 		t.Fail()
