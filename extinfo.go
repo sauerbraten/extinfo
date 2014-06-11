0b3dba34e3c99a6253d83f93eab6a8d82e43d2ae
sauerbraten <willing.alexander@googlemail.com>
2014-06-12 00:00:07 +0200

    reverted extinfo version constant to decimal literal; shorter byte literals

--- extinfo.go
+++ extinfo.go
@@ -9,18 +9,18 @@ import (
 // Protocol constants
 const (
 	// Constants describing the type of information to query for
-	EXTENDED_INFO = 0x00 //0
-	BASIC_INFO    = 0x11 //1
+	EXTENDED_INFO = 0x0 //0
+	BASIC_INFO    = 0x1 //1
 
 	EXTENDED_INFO_ACK      = 0xFF // -1
-	EXTENDED_INFO_VERSION  = 0x69 //105
-	EXTENDED_INFO_ERROR    = 0x01 //1
-	EXTENDED_INFO_NO_ERROR = 0x00 //0
+	EXTENDED_INFO_VERSION  = 105
+	EXTENDED_INFO_ERROR    = 0x1 //1
+	EXTENDED_INFO_NO_ERROR = 0x0 //0
 
 	// Constants describing the type of extended information to query for
-	EXTENDED_INFO_UPTIME       = 0x00 //0
-	EXTENDED_INFO_PLAYER_STATS = 0x01 //1
-	EXTENDED_INFO_TEAMS_SCORES = 0x02 //2
+	EXTENDED_INFO_UPTIME       = 0x0 //0
+	EXTENDED_INFO_PLAYER_STATS = 0x1 //1
+	EXTENDED_INFO_TEAMS_SCORES = 0x2 //2
 
 	EXTENDED_INFO_PLAYER_STATS_RESPONSE_IDS   = 0xF6 //-10
 	EXTENDED_INFO_PLAYER_STATS_RESPONSE_STATS = 0xF5 //-11
